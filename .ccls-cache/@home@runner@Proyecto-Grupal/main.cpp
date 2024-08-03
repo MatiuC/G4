48d4bf0caa6224134991bf99964b1b60f7a9335b
Eduardo <169008489+MatiuC@users.noreply.github.com>
2024-08-03 23:06:14 +0000

    v1

--- .ccls-cache/@home@runner@Proyecto-Grupal/main.cpp
+++ .ccls-cache/@home@runner@Proyecto-Grupal/main.cpp
@@ -1,7 +1,13 @@
 //Sistema de biblioteca (libros, autores, usuario)
-
+//Lista de funcionalidades
+/*Búsqueda de información y menús (es decir la parte que el usuario puede ingresar lo que quiere buscar y tiene que mostrar) 
+  - Almacenamiento de datos (eliminar, modificar y añadir)
+  - ⁠Presentación del programa (interfaces, mostrar resultados de manera linda)*/
 #include <iostream>
+using namespace std;
+
 
 int main() {
-  std::cout << "Hello World!\n";
+  
+  return 0;
 }
